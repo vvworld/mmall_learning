dca9ee2153569d0fcedf4db6115bfb2fdbdfb185
huangxiaowei <921169792@qq.com>
2018-07-14 00:13:40 +0800

    aliyun deploy

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -10,7 +10,6 @@ import com.mmall.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.security.provider.MD4;
 
 import java.util.UUID;
 
